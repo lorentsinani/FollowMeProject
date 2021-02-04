1b0d8d35a3b2fa8a271a78a7f78a3f344501a924
Lorik Mustafa <77617510+lorikmustafa@users.noreply.github.com>
2021-02-04 23:25:25 +0100

    Update ListaClipHeader.h

--- source/ListaClipHeader.h
+++ source/ListaClipHeader.h
@@ -3,35 +3,22 @@
 #include <iostream>
 #include "ClipHeader.h"
 
-	// Klasa "ListaClip". Struktura e te dhenave qe permban listen e klipeve.
-
-class ListaClip 
+class ListaClip        // Klasa "ListaClip". Struktura e te dhenave qe permban listen e klipeve.
 {
 public:
 	ListaClip(); //konstruktori i randomt
 	
-	 // funksioni per te nderfutur klipe
-	 
-	void addClip(Clip* c);
+	void addClip(Clip* c);  // funksioni per te nderfutur klipe te reja
 	
-	//  kthen ne poziten ekzistuese ( nese ekziston )
+	Clip* exists(std::string);   //  kthen ne poziten ekzistuese ( nese ekziston )
 	
-	Clip* exists(std::string);
-	/*
-	 *  Gets stored head pointer
-	 *  @return pointer to Clip list
-	 */
 	Clip* getHead();
 	
-	// shfaq listen e klipeve
+	void display(); // shfaq listen e klipeve
 	
-	void display();
-	/*
-	 *  ekzekuton aksionin varesisht nga probabiliteti
-	 *  varesisht se a eshte jap numer random apo i determinuar
-	 */
-	void executeAction();
-	void executeAction(float, float);
+	void executeAction();        // ekzekuton aksionin varesisht nga probabiliteti
+	
+	void executeAction(float, float);    // varesisht se a eshte jap numer random apo i determinuar
 
 private:
 	Clip* head; // pointer - per te derguar ne krye
